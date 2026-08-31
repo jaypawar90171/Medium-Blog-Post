a3040612063cf67d8c9a41ef760b31c0cb3f2335
Jay Pawar <jayp90171@gmail.com>
2026-08-31 12:32:51 +0530

    configured tailwind for styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './Screens/Landing';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/Screens/Landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
+export default Landing
